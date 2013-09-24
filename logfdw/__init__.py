7411e7359fec9f391f759cf99e9a5ae7d186db3e
Ronan Dunklau <ronan@dunklau.fr>
2013-09-24 13:11:10 +0200

    Basic version.

--- logfdw/__init__.py
+++ logfdw/__init__.py
@@ -1,6 +1,22 @@
 from multicorn import ForeignDataWrapper
+import re
 
 class LogFDW(ForeignDataWrapper):
 
+    def __init__(self, fdw_options, fdw_columns):
+        super(LogFDW, self).__init__(fdw_options, fdw_columns)
+        self.log_file = fdw_options.get('log_file', None)
+        if self.log_file is None:
+            raise ValueError('The log_file option is mandatory')
+        # Default to matching the whole line.
+        self.line_re = re.compile(fdw_options.get('line_pattern', "(.*)"))
+        if len(fdw_columns) != self.line_re.groups:
+            raise ValueError('The table should have as much columns as '
+                             'there are groups in the pattern')
+
     def execute(self, quals, columns):
-        pass
+        with open(self.log_file, 'r') as f:
+            for line in f:
+                match = self.line_re.match(line)
+                if match:
+                    yield match.groups()
--- test.sql
+++ test.sql
@@ -1,10 +1,29 @@
+DROP EXTENSION multicorn cascade;
+
+CREATE EXTENSION multicorn;
+
 CREATE SERVER log_server
   FOREIGN DATA WRAPPER multicorn
   OPTIONS (wrapper 'logfdw.LogFDW');
 
 CREATE FOREIGN TABLE logtable (
-  ts TIMESTAMP,
   message VARCHAR
-) SERVER log_server;
+) SERVER log_server OPTIONS (
+  log_file './log'
+);
 
-SELECT * from logtable;
+-- SELECT * from logtable;
+
+SELECT count(1) from logtable;
+
+CREATE FOREIGN TABLE logtable_with_severity (
+  severity VARCHAR,
+  message VARCHAR
+) SERVER log_server OPTIONS (
+  log_file './log',
+  line_pattern '^(?:(\w*):) (.*)'
+);
+
+SELECT * from logtable_with_severity;
+
+SELECT distinct severity from logtable_with_severity;
